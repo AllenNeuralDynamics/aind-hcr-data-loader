bb696488d503269476f444ad20c863a73bf1d0e2
Matthew J. Davis <mattjdavis@gmail.com>
2025-07-11 17:21:53 -0700

    remove logger

--- src/aind_hcr_data_loader/hcr_dataset.py
+++ src/aind_hcr_data_loader/hcr_dataset.py
@@ -272,7 +272,6 @@ class HCRRound:
         elif resolution_key == "2":
             # sometimes the original resolution mask is stored in a group....
             zarr_file = zarr.open(mask_path, mode="r")
-            logger.debug(f"Loaded zarr file: {zarr_file}")
             if isinstance(zarr_file, zarr.Array):
                 return zarr_file
             elif isinstance(zarr_file, zarr.Group):
